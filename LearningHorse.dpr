11bbc2f489b61b15073043690804fb100708e50c
Igor <89994712+ownerigor@users.noreply.github.com>
2023-03-12 19:23:36 -0300

    Get, Post and Delete, OK!

--- LearningHorse.dpr
+++ LearningHorse.dpr
@@ -4,14 +4,45 @@ program LearningHorse;
 
 {$R *.res}
 
-uses Horse;
+uses
+  Horse,
+  Horse.Jhonson,
+  Horse.Commons,
+  System.JSON,
+  System.SysUtils;
+
+var
+  Users: TJSONArray;
 
 begin
-  THorse.Get('/ping',
-    procedure(Req: THorseRequest; Res: THorseResponse)
-    begin
-      Res.Send('pong');
-    end);
+  Users := TJSONArray.Create;
+  THorse.Use(Jhonson());
+
+  THorse.Get('/users',
+  procedure(Req: THorseRequest; Res: THorseResponse)
+  begin
+    Res.Send<TJSONArray>(Users);
+  end);
+
+  THorse.Post('/users',
+  procedure(Req: THorseRequest; Res: THorseResponse)
+  var
+    User: TJSONObject;
+  begin
+    User := Req.Body<TJSONObject>.Clone as TJSONObject;
+    Users.AddElement(User);
+    Res.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
+  end);
+
+  THorse.Delete('/users/:id',
+  procedure(Req: THorseRequest; Res: THorseResponse)
+  var
+    Id: Integer;
+  begin
+    Id := Req.Params.Items['id'].ToInteger;
+    Users.Remove(Pred(Id)).Free;
+    Res.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.NoContent);
+  end);
 
   THorse.Listen(9000);
 end.
